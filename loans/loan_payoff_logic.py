dce3eec9565a490182a7e9a38a8fd0b3ba602a8f
Zack Walker <zwalker2016@gmail.com>
2020-05-22 23:50:46 -0500

    messing with environment variables

--- loans/loan_payoff_logic.py
+++ loans/loan_payoff_logic.py
@@ -13,8 +13,9 @@ WF1_Loan = [8250.25,.0774/12,84.62,"WF1"]
 WF2_Loan = [20000.29,.0649/12,191.04,"WF2"]
 test123 = [4000,.0480/12,70,"Test"]
 mortgage = [180000,.04/12,1331.44,"mortgage"]
-loan_list = [WF1_Loan,Lisa_Car,WF2_Loan,test123,mortgage]
+loan_list = [WF1_Loan,Lisa_Car,WF2_Loan]
 
+np.warnings.filterwarnings('ignore')
 data = []
 interest_comparsion = ['Interest']
 period_comparsion = ['Months']
@@ -25,10 +26,63 @@ def drop_items(df,items_to_drop):
         df= df.drop(item, axis=1)
     return df
 
+def get_payments(perms, extra_money):
+
+    payoff_dict = {}
+    num_loans = len(perms)
+    perms = dict(enumerate(sorted(permutations(perms))))
+
+    for perm_item in range(len(perms)):
+        payoff_list = []
+                #for each perm, this grabs the current NPER.
+                #It gives the first loan the extra amount and gets NPER based off min_payment for remaining loans
+
+        for i in range(len(perms[perm_item])):
+            rate = perms[perm_item][i][1]
+            if i == 0:
+                first_payment = perms[perm_item][i][2] + extra_money
+            else:
+                first_payment = perms[perm_item][i][2]
+            first_principal = perms[perm_item][i][0]
+            test = np.nper(rate,first_payment,first_principal)
+            payoff_list.append(math.ceil(np.nper(rate,first_payment,-first_principal)))
+            payoff_dict[perm_item] = payoff_list
+
+    payment_dict = {}
+
+    for loan_combo in range(len(payoff_dict)):
+
+        test_dict = {}
+        for iter,num in enumerate(payoff_dict[loan_combo]):
+            temporary_list = [] # create temp list
+            payment_amount = perms[loan_combo][iter][2] #ggrab payment for that loan
+            temporary_list.append(payment_amount) #append to a temp list
+            test_dict[iter] = temporary_list * num #multiply it by the nper from the previous loop so we have a list of each payment per loan
+
+            payment_dict[loan_combo] = test_dict
+            temp_add_list = []
+        for payment_list in payment_dict[loan_combo]:
+            try:
+                if len(payment_dict[loan_combo][payment_list]) > len(payment_dict[loan_combo][payment_list+1]):
+                    length_longer = len(payment_dict[loan_combo][payment_list])
+                    length_shorter = len(payment_dict[loan_combo][payment_list+1])
+                    difference = length_longer - length_shorter
+                    longer_payoff_amt = payment_dict[loan_combo][payment_list][0]
+                    shorter_payoff_amt = payment_dict[loan_combo][payment_list+1][0]
+                    temp_add_list.append(shorter_payoff_amt + longer_payoff_amt)
+                    test_vector =  temp_add_list * difference
+                    payment_dict[loan_combo][payment_list][(length_shorter):] = test_vector
+            except:
+                pass
+
+
+    return payoff_dict,payment_dict
+
 #dave ramsey, minimize interest, least number of periods, snow avalanche, lowest value in payments
 def Loan_payoff(perms,extra_money,payoff_style):
     #initiate variabes
 
+    payoff_dict, payoff_var = get_payments(perms, extra_money)
     perms = dict(enumerate(sorted(permutations(perms))))
     df = pd.DataFrame()
 
@@ -37,6 +91,7 @@ def Loan_payoff(perms,extra_money,payoff_style):
     temp_list = []
     sec_try = []
     for loan_combo in range(len(perms)):
+
         excess = 0
         agg_payment = 0
         num_periods = 1
@@ -44,18 +99,11 @@ def Loan_payoff(perms,extra_money,payoff_style):
         outer_index += 1
 
         for loan_attr in range(len(perms[loan_combo])):
-            if loan_attr == 0:
-                monthly_payment = perms[loan_combo][loan_attr][2] + extra_money
-            else:
-                monthly_payment = perms[loan_combo][loan_attr][2]
 
             principal = perms[loan_combo][loan_attr][0]
             interest_rate = perms[loan_combo][loan_attr][1]
             name = perms[loan_combo][loan_attr][3]
 
-            nper = -(math.log10(1-principal*interest_rate/monthly_payment))/(math.log10(1+interest_rate))
-            upper = math.ceil(nper)
-            period = list(range(upper))
             prin = [principal]
             payment = []
             i = []
@@ -65,8 +113,15 @@ def Loan_payoff(perms,extra_money,payoff_style):
             index_track = []
             inner_loop_index =[]
             inner_index += 1
-            for per in range(upper):
+            nper = payoff_dict[loan_combo][loan_attr]
+            upper = math.ceil(nper)
+            period = list(range(upper))
 
+            for per in range(upper):
+                if loan_attr == 0:
+                    monthly_payment = payoff_var[loan_combo][loan_attr][per] + extra_money
+                else:
+                    monthly_payment = payoff_var[loan_combo][loan_attr][per]
                 if prin[-1] > 0:
 
                     if per == num_periods-1:
@@ -172,7 +227,7 @@ def needed_index(df):
         needed_index = list(df.index.values)
         return needed_index
 
-def payoff_optimization(payoff_style,avalanche_order, df):
+def payoff_optimization(payoff_style,avalanche_order, df,base_case_flag):
         df.columns = ["Period", "Principal", "Payment"
                     ,"Interest","Amount_Towards_Principal","Ending Balance"
                     ,"Loan_Name", "Index","Inner_Loop_Iteration"]
@@ -207,49 +262,58 @@ def payoff_optimization(payoff_style,avalanche_order, df):
         # df.to_csv("C:\\Users\\zwalk\\Documents\\Desktop\\sentdex\\Loan_Payments\\final.csv")
         # dropped_columns = ['Period','Principal','Payment','Ending Balance']
         summary_df = df.copy()
-        #quickest
-        quick_pers = summary_df.sort_values(by=['max_periods','Total']).head(1) # orders by periods and then total
-        needed_index = list(quick_pers.index.values)
-        quick_pers_index = df[df['index_dup'] == needed_index[0]]
-
-        #least interest
-        least_int = summary_df.sort_values(by=['Total','max_periods']).head(1) #orders by total then by periods
-        needed_index = list(least_int.index.values)
-
-        #base_case
-        base_case_df = summary_df.sort_values(by=['max_periods','Total'],ascending=[False, False]).head(1) # orders by periods and then total
-        needed_index = list(base_case_df.index.values)
-
-        dropped_columns = ['Principal','Payment','Interest']
-        summary_df = drop_items(df,dropped_columns)
-        summary_df['count_index'] = summary_df.groupby(['Index'])['index_dup'].count()
-        #filter to last instance to get final balance
-        summary_df = summary_df[summary_df['final_bal']==summary_df['count_index']]
-        summary_df['total_payed'] = total_payment * summary_df['max_periods'] - summary_df['Ending Balance']
-
-        summary_df = summary_df.sort_values(by=['total_payed','max_periods']).head(1)
-        oop_index = list(summary_df.index_dup.values)
-
-        if total_indeces < 1:
-            dave_ramsey(df,1,total_payment)
-            dave_ramsey(df,1,total_payment)
-            dave_ramsey(df,1,total_payment)
-            dave_ramsey(df,1,total_payment)
-            dave_ramsey(df,1,total_payment)
-            dave_ramsey(df,1,total_payment)
-        else:
+        print(base_case_flag)
+        if base_case_flag:
+            #base_case
+            base_case_df = summary_df.sort_values(by=['max_periods','Total'],ascending=[False, False]).head(1) # orders by periods and then total
+            needed_index = list(base_case_df.index.values)
+            print(needed_index)
             base_case(df,needed_index[0],total_payment)
-            dave_ramsey(df,1,total_payment)
-            debt_avalanche(df,index_match,total_payment)
-            quickest_periods(df,needed_index[0],total_payment)
-            lowest_interest(df,needed_index[0],total_payment)
-            least_oop(df,oop_index[0],total_payment)
+            # print(interest_comparsion)
+            # data.append(interest_comparsion)
+            # print(data)
+            # data.append(period_comparsion)
+            # data.append(out_of_pocket_comparsion)
+
+        else:
+            #quickest
+            quick_pers = summary_df.sort_values(by=['max_periods','Total']).head(1) # orders by periods and then total
+            needed_index = list(quick_pers.index.values)
+            quick_pers_index = df[df['index_dup'] == needed_index[0]]
+
+            #least interest
+            least_int = summary_df.sort_values(by=['Total','max_periods']).head(1) #orders by total then by periods
+            needed_index = list(least_int.index.values)
+
+            dropped_columns = ['Principal','Payment','Interest']
+            summary_df = drop_items(df,dropped_columns)
+            summary_df['count_index'] = summary_df.groupby(['Index'])['index_dup'].count()
+            #filter to last instance to get final balance
+            summary_df = summary_df[summary_df['final_bal']==summary_df['count_index']]
+            summary_df['total_payed'] = total_payment * summary_df['max_periods'] - summary_df['Ending Balance']
+
+            summary_df = summary_df.sort_values(by=['total_payed','max_periods']).head(1)
+            oop_index = list(summary_df.index_dup.values)
+
+            if total_indeces < 1:
+                dave_ramsey(df,1,total_payment)
+                dave_ramsey(df,1,total_payment)
+                dave_ramsey(df,1,total_payment)
+                dave_ramsey(df,1,total_payment)
+                dave_ramsey(df,1,total_payment)
+                dave_ramsey(df,1,total_payment)
+            else:
+                dave_ramsey(df,1,total_payment)
+                debt_avalanche(df,index_match,total_payment)
+                quickest_periods(df,needed_index[0],total_payment)
+                lowest_interest(df,needed_index[0],total_payment)
+                least_oop(df,oop_index[0],total_payment)
 
         data.append(interest_comparsion)
         data.append(period_comparsion)
         data.append(out_of_pocket_comparsion)
 
-        return data
+        return data[0:3]
 
 def get_highest_period(df):
     highest_period = df['Period'].max()
@@ -277,6 +341,10 @@ def master_func(perms,extra_money,payoff_style):
     for i in range(len(perms)):
         avalanche_order.append(perms[dict_on_interest_rates[i][0]][3])
     amortization = Loan_payoff(perms,extra_money,payoff_style)
-    return payoff_optimization(payoff_style,avalanche_order,amortization)
+    amortization_base = Loan_payoff(perms,0,payoff_style)
+    payoff_optimization(payoff_style,avalanche_order,amortization_base,True)
+    # print(payoff_optimization(payoff_style,avalanche_order,amortization_base,True))
+    # print(payoff_optimization(payoff_style,avalanche_order,amortization,False))
+    return payoff_optimization(payoff_style,avalanche_order,amortization,False)
 
-# master_func(loan_list,4000,'Least Total')
+master_func(loan_list,1000,'Least Total')
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,9 +126,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'zwalker2016@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = env("SECRET_KEY")
