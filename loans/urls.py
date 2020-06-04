962e049cf0ba3c0376ebe005a00e898614cb7177
zackwalker <zwalker2016@gmail.com>
2020-06-03 21:59:48 -0500

    customized password reset pages

--- loans/urls.py
+++ loans/urls.py
@@ -11,15 +11,7 @@ urlpatterns = [
     path('<int:id>/delete', views.LoanDeleteView.as_view(), name='loan-delete'),
     path('<int:id>/update/', views.LoanUpdateView.as_view(), name='loan-update'),
     path('password_reset/',auth_views.PasswordResetView.as_view(template_name='loans/password_reset.html'),name='password_reset'),
-    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name='loans/password_reset_sent.html'),name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
-    # path('reset_password/',auth_views.PasswordResetView.as_view(template_name='loans/password_reset.html'),
-    #     name='reset_password'),
-    # path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name='loans/password_reset_sent.html'),
-    #     name='password_reset_done'),
-    # path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='loans/password_reset.html'),
-    #     name='password_reset_confirm'),
-    # path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='loans/password_reset.html'),
-    #     name='password_reset_complete')
+    path('accounts/password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='loans/password_reset_sent.html'),name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='loans/password_reset_form.html'),name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='loans/password_reset_complete.html'),name='password_reset_complete'),
 ]
